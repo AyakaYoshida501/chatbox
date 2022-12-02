0cd067b4ae1a2c2fc975364881946d96e49fea6f
Ayaka Yoshida <aykhk5@gmail.com>
2022-12-02 14:43:50 +0900

    編集画面修正

--- next/pages/edit.tsx
+++ next/pages/edit.tsx
@@ -26,7 +26,28 @@ export default function Home(resOfHistory: any) {
             console.log(res);
         })
     }
-    {console.log(resOfHistory.resOfHistory[0].His)}
+    const showHistory  = () => {
+        if(resOfHistory.resOfHistory[0].His) {
+            return resOfHistory.resOfHistory[0].His
+        }
+    }
+    // {console.log(resOfHistory.resOfHistory[0].His)}
+//     const plusTextarea = () => { //skillの入力タグ追加用
+//         const skills: HTMLElement | null = document.querySelector<HTMLElement>(".plusSkill")
+//         const pulsTag:HTMLInputElement = document.createElement('input');//<input className='skill' cols={50} rows={10} value={skill} onChange={(e) => setSkill(e.target.value)}></input>
+//         pulsTag.type = "text";
+//         pulsTag.className = "pulsTag"
+//         pulsTag.value = skill;
+//         onchange=(e) => setSkill(e.target.value)}
+//         pulsTag.onmouseover = function(event:MouseEvent) { // ここ
+//         alert(event);
+// }
+//         document.body.appendChild(pulsTag)
+//         console.log("test")
+//         return (
+//             <textarea className='skill' cols={50} rows={10} value={skill} onChange={(e) => setSkill(e.target.value)}></textarea>
+//         )
+//     }
     return (
         <div className='container'>
             <Head>
@@ -44,7 +65,7 @@ export default function Home(resOfHistory: any) {
                         <textarea className='history' cols={50} rows={10} value={history} onChange={(e) => setHistory(e.target.value)}></textarea>
                         <button className='postHistoryBtn' onClick={postHistory}>決定</button>
                     </div>
-                    {resOfHistory.resOfHistory[0].His}
+                    {showHistory()}
                 </div>
                 <div className='projects'>
                     <h2>作ったもの</h2>
@@ -56,22 +77,13 @@ export default function Home(resOfHistory: any) {
                 <div className='skills'>
                     <h2>skills</h2>
                     <div text-align="left">{/*imgタグ１つ準備して、srcの中身をAPIで拾ってループ回す */}
-                    <div className='myHistoryEdit'>
-                        <textarea className='skill' cols={50} rows={10} value={skill} onChange={(e) => setSkill(e.target.value)}></textarea>
+                    <div className='myskillsEdit'>
+                        <div className='plusSkill'>
+                            <textarea className='skill' cols={50} rows={10} value={skill} onChange={(e) => setSkill(e.target.value)}></textarea>
+                        </div>
+                        {/* <button className='plusTextarea' onClick={plusTextarea}>入力欄追加</button> */}
                         <button className='postHSkillBtn' onClick={postSkill}>送信する</button>
                     </div>
-
-                    {/* <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg" alt="typescript" width="40" height="40"/>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original-wordmark.svg" alt="go" width="40" height="40"/> 
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/php/php-original.svg" alt="php" width="40" height="40"/>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nextjs/nextjs-original-wordmark.svg" alt="next.js" width="40" height="40"/>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> 
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" alt="mysql" width="40" height="40"/>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain-wordmark.svg" alt="firebase" width="40" height="40"/>
-                    <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg" alt="docker" width="40" height="40"/> */}
                     </div>
                 </div>
             </main>
@@ -83,6 +95,7 @@ export default function Home(resOfHistory: any) {
 export async function getServerSideProps() {
     const res = await axios.get(`${process.env.API}/getHistories`, {
     });
+    // const iconRes = 
     const resOfHistory = await res.data;
     // {console.log(history)}
     // {console.log(typeof history)}
