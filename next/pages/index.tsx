d9712631b1de9396dfd419b3c8f446f57e55bc62
Ayaka Yoshida <aykhk5@gmail.com>
2023-01-06 19:41:57 +0900

    footer削除

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -66,7 +66,7 @@ const showIcons = () => {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -77,7 +77,7 @@ const showIcons = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
